9f923815fa6d136022c778e56b5120c0098b8d6f
Rentacc <124709345+Rentacc@users.noreply.github.com>
2023-04-25 21:20:28 +0200

    add eventlistners

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ const eleGrid = document.querySelector('.grid');
 for(let i = 0; i < 100; i++){
 eleGrid.innerHTML += '<div class="cell"></div>'
 }
+
+
+const listcells = document.querySelectorAll('.cell');
+
+for (let i = 0; i < listcells.length; i++){
+     const cell = listcells[i];
+     cell.addEventListener('click' , 
+     function(){
+     console.log([i]);
+     this.classList.toggle('clicked')
+     }
+     );
+}
